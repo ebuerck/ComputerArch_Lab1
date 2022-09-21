198b46b47e9ff221bdcc5528be2a91585481dbd1
Ethan Buerck <ebuerck@gmail.com>
2022-09-21 10:17:48 -0500

    Removed debugging added some commits and retrofitted I

--- src/mu-mips.c
+++ src/mu-mips.c
@@ -11,7 +11,7 @@
 /***************************************************************/
 /* Print out a list of commands available                                                                  */
 /***************************************************************/
-void help() {        
+void help() {
 	printf("------------------------------------------------------------------\n\n");
 	printf("\t**********MU-MIPS Help MENU**********\n\n");
 	printf("sim\t-- simulate program to completion \n");
@@ -68,7 +68,7 @@ void mem_write_32(uint32_t address, uint32_t value)
 /***************************************************************/
 /* Execute one cycle                                                                                                              */
 /***************************************************************/
-void cycle() {                                                
+void cycle() {
 	handle_instruction();
 	CURRENT_STATE = NEXT_STATE;
 	INSTRUCTION_COUNT++;
@@ -77,8 +77,8 @@ void cycle() {
 /***************************************************************/
 /* Simulate MIPS for n cycles                                                                                       */
 /***************************************************************/
-void run(int num_cycles) {                                      
-	
+void run(int num_cycles) {
+
 	if (RUN_FLAG == FALSE) {
 		printf("Simulation Stopped\n\n");
 		return;
@@ -98,7 +98,7 @@ void run(int num_cycles) {
 /***************************************************************/
 /* simulate to completion                                                                                               */
 /***************************************************************/
-void runAll() {                                                     
+void runAll() {
 	if (RUN_FLAG == FALSE) {
 		printf("Simulation Stopped.\n\n");
 		return;
@@ -111,10 +111,10 @@ void runAll() {
 	printf("Simulation Finished.\n\n");
 }
 
-/***************************************************************/ 
+/***************************************************************/
 /* Dump a word-aligned region of memory to the terminal                              */
 /***************************************************************/
-void mdump(uint32_t start, uint32_t stop) {          
+void mdump(uint32_t start, uint32_t stop) {
 	uint32_t address;
 
 	printf("-------------------------------------------------------------\n");
@@ -128,10 +128,10 @@ void mdump(uint32_t start, uint32_t stop) {
 }
 
 /***************************************************************/
-/* Dump current values of registers to the teminal                                              */   
+/* Dump current values of registers to the teminal                                              */
 /***************************************************************/
-void rdump() {                               
-	int i; 
+void rdump() {
+	int i;
 	printf("-------------------------------------\n");
 	printf("Dumping Register Content\n");
 	printf("-------------------------------------\n");
@@ -150,9 +150,9 @@ void rdump() {
 }
 
 /***************************************************************/
-/* Read a command from standard input.                                                               */  
+/* Read a command from standard input.                                                               */
 /***************************************************************/
-void handle_command() {                         
+void handle_command() {
 	char buffer[20];
 	uint32_t start, stop, cycles;
 	uint32_t register_no;
@@ -168,7 +168,7 @@ void handle_command() {
 	switch(buffer[0]) {
 		case 'S':
 		case 's':
-			runAll(); 
+			runAll();
 			break;
 		case 'M':
 		case 'm':
@@ -213,8 +213,8 @@ void handle_command() {
 			if (scanf("%i", &hi_reg_value) != 1){
 				break;
 			}
-			CURRENT_STATE.HI = hi_reg_value; 
-			NEXT_STATE.HI = hi_reg_value; 
+			CURRENT_STATE.HI = hi_reg_value;
+			NEXT_STATE.HI = hi_reg_value;
 			break;
 		case 'L':
 		case 'l':
@@ -226,7 +226,7 @@ void handle_command() {
 			break;
 		case 'P':
 		case 'p':
-			print_program(); 
+			print_program();
 			break;
 		default:
 			printf("Invalid Command.\n");
@@ -237,7 +237,7 @@ void handle_command() {
 /***************************************************************/
 /* reset registers/memory and reload program                                                    */
 /***************************************************************/
-void reset() {   
+void reset() {
 	int i;
 	/*reset registers*/
 	for (i = 0; i < MIPS_REGS; i++){
@@ -245,15 +245,15 @@ void reset() {
 	}
 	CURRENT_STATE.HI = 0;
 	CURRENT_STATE.LO = 0;
-	
+
 	for (i = 0; i < NUM_MEM_REGION; i++) {
 		uint32_t region_size = MEM_REGIONS[i].end - MEM_REGIONS[i].begin + 1;
 		memset(MEM_REGIONS[i].mem, 0, region_size);
 	}
-	
+
 	/*load program*/
 	load_program();
-	
+
 	/*reset PC*/
 	INSTRUCTION_COUNT = 0;
 	CURRENT_STATE.PC =  MEM_TEXT_BEGIN;
@@ -264,7 +264,7 @@ void reset() {
 /***************************************************************/
 /* Allocate and set memory to zero                                                                            */
 /***************************************************************/
-void init_memory() {                                           
+void init_memory() {
 	int i;
 	for (i = 0; i < NUM_MEM_REGION; i++) {
 		uint32_t region_size = MEM_REGIONS[i].end - MEM_REGIONS[i].begin + 1;
@@ -276,7 +276,7 @@ void init_memory() {
 /**************************************************************/
 /* load program into memory                                                                                      */
 /**************************************************************/
-void load_program() {                   
+void load_program() {
 	FILE * fp;
 	int i, word;
 	uint32_t address;
@@ -303,7 +303,7 @@ void load_program() {
 }
 
 /************************************************************/
-/* decode and execute instruction                                                                     */ 
+/* decode and execute instruction                                                                     */
 /************************************************************/
 void handle_instruction()
 {
@@ -313,9 +313,9 @@ void handle_instruction()
 
 
 /************************************************************/
-/* Initialize Memory                                                                                                    */ 
+/* Initialize Memory                                                                                                    */
 /************************************************************/
-void initialize() { 
+void initialize() {
 	init_memory();
 	CURRENT_STATE.PC = MEM_TEXT_BEGIN;
 	NEXT_STATE = CURRENT_STATE;
@@ -323,12 +323,12 @@ void initialize() {
 }
 
 /************************************************************/
-/* Print the program loaded into memory (in MIPS assembly format)    */ 
+/* Print the program loaded into memory (in MIPS assembly format)    */
 /************************************************************/
 void print_program(){
 	int i;
 	uint32_t addr;
-	
+
 	for(i=0; i<PROGRAM_SIZE; i++){
 		addr = MEM_TEXT_BEGIN + (i*4);
 		printf("[0x%x]\t", addr);
@@ -376,7 +376,7 @@ char* hex_to_binary(char Hexdigit)
 	case 'E':
 		return "1110";
 	case 'F':
-		return "1111";	
+		return "1111";
 	default:
 		return "\0";
 		break;
@@ -562,11 +562,11 @@ char* GetIFunction(char* instruction, char* rt)
 	{
 		return "BLEZ";
 	}
-	if(!strcmp(instruction, "000001") && !strcmp(rt, "00000")) 
+	if(!strcmp(instruction, "000001") && !strcmp(rt, "00000"))
 	{
 		return "BLTZ";
 	}
-	if(!strcmp(instruction, "000001") && !strcmp(rt, "00001")) 
+	if(!strcmp(instruction, "000001") && !strcmp(rt, "00001"))
 	{
 		return "BGEZ";
 	}
@@ -595,38 +595,39 @@ char* GetJFunction(char* instruction)
 }
 
 int convertBinarytoDecimal(char * binary) {
-	int bin = atoi(binary);
+	unsigned int bin = atoi(binary);
 
-  int dec = 0, i = 0, rem;
+  	unsigned int dec = 0, i = 0, rem;
 
 
-  while (bin!=0) {
-    rem = bin % 10;
-    bin /= 10;
-    dec += rem * i * i;
-    ++i;
-  }
+  	while (bin!=0) {
+    	rem = bin % 10;
+    	bin /= 10;
+    	dec += rem * i * i;
+    	++i;
+  	}
 
   return dec;
 }
 
 
 char* returnRFormat(char* instruction) {
+	// Read in the rs register
 	char rs[6];
-	printf("fullbinay is %s\n", instruction);
 	strncpy(rs, &instruction[6], 5);
 	rs[5] = '\0';
-	printf("rs is %s\n", rs);
-	printf("The rs is %s\n", returnRegister(rs));
+
+	// Read in the rt register
 	char rt[6];
 	strncpy(rt, &instruction[11], 5);
 	rt[5] = '\0';
-	printf("rt is %s\n", rt);
+
+	// read in the rd register
 	char rd[6];
 	strncpy(rd, &instruction[16], 5);
 	rd[5] = '\0';
-	printf("rd is %s\n", rd);
 
+	// read in the op code
 	char op[7];
 	strncpy(op, &instruction[0], 6);
 	op[6] = '\0';
@@ -636,33 +637,57 @@ char* returnRFormat(char* instruction) {
 }
 
 char* returnIFormat(char* instruction) {
+	// read in the rs register
 	char rs[6];
-	printf("fullbinay is %s\n", instruction);
 	strncpy(rs, &instruction[6], 5);
 	rs[5] = '\0';
 
+	// read in the rt register
 	char rt[6];
 	strncpy(rt, &instruction[11], 5);
 	rt[5] = '\0';
-	
+
+	// read in the imm value and convert to decimal
 	char imm[17];
 	strncpy(imm, &instruction[16], 16);
 	imm[16] = '\0';
-	
+	int imm_decimal = convertBinarytoDecimal(imm);
 
+	// read in the op code
 	char op[7];
 	strncpy(op, &instruction[0], 6);
 	op[6] = '\0';
-	int decimal = convertBinarytoDecimal(imm);
 
-	printf("%s %s, %s, %d\n",GetIFunction(op, rt),returnRegister(rt), returnRegister(rs), decimal);
+	if(!strcmp(GetIFunction(op, rt), "LUI"))
+	{
+		printf("%s %s, %d\n",GetIFunction(op, rt),returnRegister(rt), imm_decimal);
+	}
+	else if(!strcmp(GetIFunction(op, rt), "SW") || !strcmp(GetIFunction(op, rt), "SB") || !strcmp(GetIFunction(op, rt), "SH"))
+	{
+		printf("%s %s, %d(%s)\n",GetIFunction(op, rt),returnRegister(rs),imm_decimal,returnRegister(rt));
+	}
+	else if(!strcmp(GetIFunction(op, rt), "LW"))
+	{
+		printf("%s %s, %d(%s)\n",GetIFunction(op, rt),returnRegister(rt),imm_decimal,returnRegister(rs));
+	}
+	else if(!strcmp(GetIFunction(op, rt), "BEQ") || !strcmp(GetIFunction(op, rt), "BNE"))
+	{
+		printf("%s %s, %s, %d\n",GetIFunction(op, rt),returnRegister(rs),returnRegister(rt), imm_decimal);
+	}
+	else
+	{
+		printf("%s %s, %s, %d\n",GetIFunction(op, rt),returnRegister(rt), returnRegister(rs), imm_decimal);
+	}
 	return NULL;
 }
 
 char* returnJFormat(char* instruction) {
+	// read in the op code
 	char op[7];
 	strncpy(op, &instruction[0], 6);
 	op[6] = '\0';
+
+	// read in the Jump Addresss
 	char address[27];
 	strncpy(address, &instruction[6], 26);
 	printf("%s %s\n", GetJFunction(op), address);
@@ -709,11 +734,8 @@ char* returnRegister(char* reg){
 /* Print the instruction at given memory address (in MIPS assembly format)    */
 /************************************************************/
 void print_instruction(uint32_t addr){
-	/*IMPLEMENT THIS*/
+	//Read in the instructions
 	uint32_t instr = mem_read_32(addr);
-
-	printf("The instruction %x\n", instr);
-	//instr = hex_to_binary(instr);
 
 	char string[9];
 	sprintf(string,"%8x", instr);
@@ -726,8 +748,6 @@ void print_instruction(uint32_t addr){
 	{
 		strcat(fullbinay, hex_to_binary(string[i]));
 	}
-
-	printf("format is %c\n", FindFormat(fullbinay));
 
 	switch(FindFormat(fullbinay)) {
 		case 'R': {
@@ -752,11 +772,11 @@ void print_instruction(uint32_t addr){
 /***************************************************************/
 /* main                                                                                                                                   */
 /***************************************************************/
-int main(int argc, char *argv[]) {                              
+int main(int argc, char *argv[]) {
 	printf("\n**************************\n");
 	printf("Welcome to MU-MIPS SIM...\n");
 	printf("**************************\n\n");
-	
+
 	if (argc < 2) {
 		printf("Error: You should provide input file.\nUsage: %s <input program> \n\n",  argv[0]);
 		exit(1);
